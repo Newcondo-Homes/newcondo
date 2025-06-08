6aad32df0e4da160340a6cfef2a13e30a8b22e99
newcondo-hq <cto@newcondo.homes>
2025-06-08 11:05:29 +0200

    vercel said use link instead of a

--- apps/platform/app/(auth)/signup/page.tsx
+++ apps/platform/app/(auth)/signup/page.tsx
@@ -1,5 +1,6 @@
 
 import {  Command } from "lucide-react"
+import Link from "next/link"
 
 import { SignUpForm } from "../_components/signupForm"
 
@@ -7,12 +8,12 @@ export default function SignupPage() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 bg-muted p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="/" className="flex items-center gap-2 self-center font-medium">
+        <Link href="/" className="flex items-center gap-2 self-center font-medium">
           <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
             <Command className="size-4" />
           </div>
           Mellon
-        </a>
+        </Link>
         <SignUpForm />
       </div>
     </div>
