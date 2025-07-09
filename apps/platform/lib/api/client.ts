f71714b3edefbd2e4c68a7d6efcbf7abca27cfe2
newcondo-hq <cto@newcondo.homes>
2025-07-09 14:23:14 +0200

    ok

--- apps/platform/lib/api/client.ts
+++ apps/platform/lib/api/client.ts
@@ -22,7 +22,7 @@ export interface ApiError {
 }
 
 const API_BASE_URL =
-  process.env.PLATFORM_PROD === `production`
+  process.env.PLATFORM_PROD! as string === `production`
     ? process.env.NEXT_PUBLIC_API_URL
     : process.env.NEXT_PUBLIC_API_URL_DEV;
 
