a0b2272def994f20a0e8dd8c4c45f97ececfca67
Nnamaka <nnamaka7@gmail.com>
2026-06-06 20:23:52 +0100

    here

--- apps/platform/components/sections/navbar.tsx
+++ apps/platform/components/sections/navbar.tsx
@@ -83,6 +83,7 @@ export function Navbar() {
         </div>
 
 
+
         <button
           
           className="nav-burger-btn data-nav-item flex ml-auto items-center justify-center p-1.5 bg-transparent border-0 cursor-pointer"
